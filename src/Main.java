185d467dd2178e4e22c5eeda4214c3673d63e545
Adam <adamboissy2004@gmail.com>
2024-12-10 17:22:14 +0100

    MD5

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,22 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import hashing.MD5Hash;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // Read user input from the terminal
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter a message to hash with MD5:");
+        String message = scanner.nextLine();
+
+        // Compute MD5 hash
+        String md5Hash = MD5Hash.calculateMD5(message);
+
+        System.out.println("MD5 Hash: " + md5Hash);
+
+        // Close the scanner
+        scanner.close();
     }
 }
