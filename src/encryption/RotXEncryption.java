0e5584ea845d381294a7cf7b4c5fa7cdf60d9f3e
ISMAILOV <elmani3107@gmail.com>
2024-12-11 16:47:59 +0100

    ROTX PUSH
    rotx method finished

--- src/encryption/RotXEncryption.java
+++ src/encryption/RotXEncryption.java
@@ -1,5 +1,42 @@
 package encryption;
 
-public class RotXEncryption {
-    
+public class RotXEncryption extends MotherEncryption {
+
+
+
+    public RotXEncryption() {
+        this.type = "ROT";
+    }
+
+
+ 
+
+    @Override
+    public String encryption(String message, String key) {
+        return rotate(message, Integer.valueOf(key));
+    }
+
+    @Override
+    public String decipher(String message, String key) {
+        return rotate(message, 26 - Integer.valueOf(key));
+    }
+
+    private String rotate(String text, int shift) {
+        StringBuilder result = new StringBuilder();
+
+        for (char c : text.toCharArray()) {
+            if (Character.isUpperCase(c)) {
+                char rotated = (char) (((c - 'A' + shift) % 26) + 'A');
+                result.append(rotated);
+            } else if (Character.isLowerCase(c)) {
+                char rotated = (char) (((c - 'a' + shift) % 26) + 'a');
+                result.append(rotated);
+            } else {
+                // Non-alphabetical characters remain unchanged
+                result.append(c);
+            }
+        }
+
+        return result.toString();
+    }
 }
