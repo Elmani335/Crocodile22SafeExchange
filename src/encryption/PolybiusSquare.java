b33572c7157f7af1126c34af30d499031fb07b37
Elman <elmani3107@gmail.com>
2024-12-13 01:50:18 +0100

    Fix polyalgo
    Fixed polybus algo char check

--- src/encryption/PolybiusSquare.java
+++ src/encryption/PolybiusSquare.java
@@ -1,19 +1,17 @@
 package encryption;
 
-public class PolybiusSquare extends MotherEncryption{
-    
-    // Initializing Polybius' square
-    static String[][] scare = {    
-        
-                {"z",   "k",    "l",    "m",    "n"},
-                {"y",   "i/j",  "b",    "c",    "o"},
-                {"x",   "h",    "a",    "d",    "p"},
-                {"w",   "g",    "f",    "e",    "q"},
-                {"v",   "u",    "t",    "s",    "r"}
-        };
+public class PolybiusSquare extends MotherEncryption {
 
+    // Initialize Polybius' square
+    static String[][] scare = {
+        {"z", "k", "l", "m", "n"},
+        {"y", "i/j", "b", "c", "o"},
+        {"x", "h", "a", "d", "p"},
+        {"w", "g", "f", "e", "q"},
+        {"v", "u", "t", "s", "r"}
+    };
 
-    public PolybiusSquare(){
+    public PolybiusSquare() {
         this.type = "Polybius";
     }
 
@@ -24,87 +22,67 @@ public class PolybiusSquare extends MotherEncryption{
      * @return String: Encrypted words
      */
     @Override
-    public String encryption(String message, String key){
-
-        System.out.println("Message to be encrypted with polybe scare: "+ message);
+    public String encryption(String message, String key) {
+        System.out.println("Message to be encrypted with polybe scare: " + message);
         StringBuilder result = new StringBuilder();
         String tmpLetter;
 
         // Message letter browsing
-        for (int i=0; i<message.length();i++){
-
+        for (int i = 0; i < message.length(); i++) {
             tmpLetter = String.valueOf(message.charAt(i));
 
-            if (tmpLetter.equals(" ")){
-                // Keep the space
+            if (tmpLetter.equals(" ")) {
                 result.append(" ");
-            }else {
-
-                String[] tmpList;
-                // Square course
-                for (int y = 0; y < scare.length; y++) {
-                    tmpList = scare[y];
-
-                    // Browse square sub-lists
-                    for (int z = 0; z < tmpList.length; z++) {
-
-                        // If we find the letter in the square, we add the coordinates
-                        if (tmpList[z].indexOf(tmpLetter) != -1) {
-                            //System.out.println("Letter found: " + tmpLetter + ", position: " + y + "," + z);
+            } else {
+                boolean found = false; // Flag to exit loops once found
+                for (int y = 0; y < scare.length && !found; y++) {
+                    for (int z = 0; z < scare[y].length; z++) {
+                        if (scare[y][z].indexOf(tmpLetter) != -1) {
                             result.append(y).append(z);
+                            found = true; // Exit loops once the letter is found
                             break;
                         }
                     }
                 }
             }
-
         }
         return result.toString();
     }
 
     /**
-     * Deciphers a message thanks to the double-dimensional board. Can receive spaces 
+     * Deciphers a message thanks to the double-dimensional board. Can receive spaces.
      * @param messageEncryption : String, message encrypted by the same array
      * @param key : String, value null
      * @return : String, message decrypted with spaces
      */
     @Override
-    public String decipher(String messageEncryption, String key){
-
-        System.out.println("Message to decipher: "+messageEncryption);
+    public String decipher(String messageEncryption, String key) {
+        System.out.println("Message to decipher: " + messageEncryption);
         StringBuilder result = new StringBuilder();
         String[] partOfMessage = messageEncryption.split(" ");
-        String x;
-        String y;
 
-        // Separate the message between each line break
-        // We then go through the strings two by two to retrieve the x and y indexes
-        // These will be used to retrieve a string from the double-dimensional array
-
-        for (String travelNumber : partOfMessage){
-
-            for (int i=0;i<travelNumber.length();i+=2){
-
-                // Directly retrieving the variable travelNumber.charAt(i) as an int does not retrieve the correct value
-                // To avoid a change when retrieving the char, we store the value in a string, which will be caster as an int
-
-                x = String.valueOf(travelNumber.charAt(i));
-                y = String.valueOf(travelNumber.charAt(i+1));
-                //System.out.println("{"+x+" "+travelNumber.charAt(i)+"} : " + y);
-                result.append(scare[Integer.valueOf(x)][Integer.valueOf(y)]);
+        for (String travelNumber : partOfMessage) {
+            if (travelNumber.length() % 2 != 0) {
+                System.out.println("Warning: Incorrect length, ignoring last character.");
+                travelNumber = travelNumber.substring(0, travelNumber.length() - 1); // Trim the last character
             }
-
+            for (int i = 0; i < travelNumber.length(); i += 2) {
+                try {
+                    int x = Integer.parseInt(String.valueOf(travelNumber.charAt(i)));
+                    int y = Integer.parseInt(String.valueOf(travelNumber.charAt(i + 1)));
+                    result.append(scare[x][y]);
+                } catch (NumberFormatException | ArrayIndexOutOfBoundsException e) {
+                    System.out.println("Decoding error at indices " + i + " and " + (i + 1));
+                }
+            }
             result.append(" ");
-
         }
-        // Delete the last excess space generated
-        result.delete(result.length(), result.length());
+        if (result.length() > 0) result.setLength(result.length() - 1); 
         return result.toString();
     }
 
     @Override
-    public Boolean checkInputUserKey(String key){
+    public Boolean checkInputUserKey(String key) {
         return true;
     }
-
 }
